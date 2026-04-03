c54a375dd6e880717fc1c58c3c186da545f2bba6
Vleena <vleenamascarenhas1998@gmail.com>
2026-04-03 15:37:54 +0530

    chnages to the jenkin file

--- Project-1/main.tf
+++ Project-1/main.tf
@@ -44,19 +44,19 @@ resource "aws_internet_gateway" "myigw" {
 
 # create route table
 resource "aws_route_table" "myroutetable" {
-    vpc_id = aws_vpc.myvpc.ip
+    vpc_id = aws_vpc.myvpc.id
 }
 
 # create a route
 resource "aws_route" "myroute" {
-    route_table_id = aws_route_table.my-route-table.id
+    route_table_id = aws_route_table.myroutetable.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my-igw.id
+    gateway_id = aws_internet_gateway.myigw.id
 }
 
 # create a route associations
 resource "aws_route_table_association" "routetableassociation" {
     subnet_id = aws_subnet.subnet1.id
-    route_table_id = aws_route_table.my-route-table.id
+    route_table_id = aws_route_table.myroutetable.id
   
 }
