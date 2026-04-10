8950484468e237016ede1d3eba2504cafbcc695f
vleena <vleenamascarenhas1998@gmail.com>
2026-04-10 14:46:46 +0530

    count the word

---
+++ SHELL-SCRIPT/if.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###################
+
+# Description: count the given character in the given word
+# Author: Vleena
+# Version:V1
+
+#####################
+set -eo pipefail
+c=0
+read -p "Enter the word:" w
+read -p "Enter the letter to be serached:" l
+# split the word into the characters
+for i in $(echo "$w" | grep -o .);
+do
+	if [[ "$i" == "$l" ]]; then
+		((c=c+1))
+	fi
+done
+
+echo "count: $c"
