ea39f74b70518d46befdc59fd02249f0699e6687
Vleena <32513883+Vleena@users.noreply.github.com>
2026-04-24 16:09:49 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(string imagename, string imagetag, string dockerusername)
+def call(String imagename, String imagetag, String dockerusername)
 {
   sh "docker build -t ${dockerusername}/${dockerusername}:${imagetag} ."
 }
